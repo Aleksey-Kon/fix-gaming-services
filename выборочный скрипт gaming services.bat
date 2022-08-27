1c2613c7d9f8ea2e6e66ad45441c36e72e83514e
Aleksey-Kon <112122036+Aleksey-Kon@users.noreply.github.com>
2022-08-27 12:26:28 +0300

    Add files via upload

--- выборочный скрипт gaming services.bat
+++ выборочный скрипт gaming services.bat
@@ -1,15 +1,15 @@
 @echo off
-title фикс gaming services
-echo Вас приветствует скрипт для фикса gaming services
-echo Сделал Алексей Кон
+title ╤Д╨╕╨║╤Б gaming services
+echo ╨Т╨░╤Б ╨┐╤А╨╕╨▓╨╡╤В╤Б╤В╨▓╤Г╨╡╤В ╤Б╨║╤А╨╕╨┐╤В ╨┤╨╗╤П ╤Д╨╕╨║╤Б╨░ gaming services
+echo ╨б╨┤╨╡╨╗╨░╨╗ ╨Р╨╗╨╡╨║╤Б╨╡╨╣ ╨Ъ╨╛╨╜
 pause
 cls
 :start
-echo 1. Удаление gaming services и SoftwareDistribution
-echo 2. Проверка системы на целостность
-echo 3. запуск wsreset
-echo 4. Установка gaming services
-set /p choice=Введите цифру 
+echo 1. ╨г╨┤╨░╨╗╨╡╨╜╨╕╨╡ gaming services ╨╕ SoftwareDistribution
+echo 2. ╨Я╤А╨╛╨▓╨╡╤А╨║╨░ ╤Б╨╕╤Б╤В╨╡╨╝╤Л ╨╜╨░ ╤Ж╨╡╨╗╨╛╤Б╤В╨╜╨╛╤Б╤В╤М
+echo 3. ╨╖╨░╨┐╤Г╤Б╨║ wsreset
+echo 4. ╨г╤Б╤В╨░╨╜╨╛╨▓╨║╨░ gaming services
+set /p choice=╨Т╨▓╨╡╨┤╨╕╤В╨╡ ╤Ж╨╕╤Д╤А╤Г 
 if '%choice%'=='1' = goto 1
 if '%choice%'=='2' = goto 2
 if '%choice%'=='3' = goto 3
@@ -18,7 +18,7 @@ goto start
 :1
 title 1
 echo ------------------------
-echo удаление Gaming Services
+echo ╤Г╨┤╨░╨╗╨╡╨╜╨╕╨╡ Gaming Services
 echo ------------------------
 powershell -ExecutionPolicy ByPass -command "get-appxpackage Microsoft.GamingServices | remove-AppxPackage -allusers"
 reg delete "HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\PolicyCache\Microsoft.GamingServices_8wekyb3d8bbwe"
@@ -27,13 +27,13 @@ reg delete "HKEY_USERS\.DEFAULT\Software\Microsoft\Windows NT\CurrentVersion\Hos
 reg delete "HKEY_USERS\S-1-5-19\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\SystemAppData\Microsoft.GamingServices_8wekyb3d8bbwe"
 reg delete "HKEY_USERS\S-1-5-19\Software\Microsoft\Windows NT\CurrentVersion\HostActivityManager\CommitHistory\Microsoft.GamingServices_8wekyb3d8bbwe!GamingServices"
 echo -----------------------------
-echo удаление SoftwareDistribution
+echo ╤Г╨┤╨░╨╗╨╡╨╜╨╕╨╡ SoftwareDistribution
 echo ----------------------------- 
 del %SystemRoot%\SoftwareDistribution
 mkdir %SystemRoot%\SoftwareDistribution
 color 2
 echo -------
-echo успешно
+echo ╤Г╤Б╨┐╨╡╤И╨╜╨╛
 echo -------
 color 7
 goto exit
@@ -41,14 +41,14 @@ goto start
 :2
 title 2
 echo -------------------------------
-echo проверка системы на целостность
+echo ╨┐╤А╨╛╨▓╨╡╤А╨║╨░ ╤Б╨╕╤Б╤В╨╡╨╝╤Л ╨╜╨░ ╤Ж╨╡╨╗╨╛╤Б╤В╨╜╨╛╤Б╤В╤М
 echo -------------------------------
 @echo on
 sfc /scannow
 @echo off
 color 2
 echo -------------------------------
-echo успешно
+echo ╤Г╤Б╨┐╨╡╤И╨╜╨╛
 echo -------------------------------
 color 7
 goto exit
@@ -56,30 +56,30 @@ goto start
 :3
 title 3
 echo --------------
-echo запуск wsreset
+echo ╨╖╨░╨┐╤Г╤Б╨║ wsreset
 echo --------------
 %SystemRoot%\System32\WSReset.exe
 color 2
 echo -------
-echo успешно
+echo ╤Г╤Б╨┐╨╡╤И╨╜╨╛
 echo -------
 color 7
 goto exit
 goto start
 :4
 title 4
-echo Вы можете попробовать скачать и установить gaming services автоматически,
-echo может не работать т.к я могу забыть обновить ссылку и будет качаться не последняя версия. Ну и она не будет работать.
-echo Либо вы можете скачать gaming services в ручную и установить их.
-echo 5. Ручная установка
-echo 6. Автоматическая установка
-set /p choice=Введите цифру 
+echo ╨Т╤Л ╨╝╨╛╨╢╨╡╤В╨╡ ╨┐╨╛╨┐╤А╨╛╨▒╨╛╨▓╨░╤В╤М ╤Б╨║╨░╤З╨░╤В╤М ╨╕ ╤Г╤Б╤В╨░╨╜╨╛╨▓╨╕╤В╤М gaming services ╨░╨▓╤В╨╛╨╝╨░╤В╨╕╤З╨╡╤Б╨║╨╕,
+echo ╨╝╨╛╨╢╨╡╤В ╨╜╨╡ ╤А╨░╨▒╨╛╤В╨░╤В╤М ╤В.╨║ ╤П ╨╝╨╛╨│╤Г ╨╖╨░╨▒╤Л╤В╤М ╨╛╨▒╨╜╨╛╨▓╨╕╤В╤М ╤Б╤Б╤Л╨╗╨║╤Г ╨╕ ╨▒╤Г╨┤╨╡╤В ╨║╨░╤З╨░╤В╤М╤Б╤П ╨╜╨╡ ╨┐╨╛╤Б╨╗╨╡╨┤╨╜╤П╤П ╨▓╨╡╤А╤Б╨╕╤П. ╨Э╤Г ╨╕ ╨╛╨╜╨░ ╨╜╨╡ ╨▒╤Г╨┤╨╡╤В ╤А╨░╨▒╨╛╤В╨░╤В╤М.
+echo ╨Ы╨╕╨▒╨╛ ╨▓╤Л ╨╝╨╛╨╢╨╡╤В╨╡ ╤Б╨║╨░╤З╨░╤В╤М gaming services ╨▓ ╤А╤Г╤З╨╜╤Г╤О ╨╕ ╤Г╤Б╤В╨░╨╜╨╛╨▓╨╕╤В╤М ╨╕╤Е.
+echo 5. ╨а╤Г╤З╨╜╨░╤П ╤Г╤Б╤В╨░╨╜╨╛╨▓╨║╨░
+echo 6. ╨Р╨▓╤В╨╛╨╝╨░╤В╨╕╤З╨╡╤Б╨║╨░╤П ╤Г╤Б╤В╨░╨╜╨╛╨▓╨║╨░
+set /p choice=╨Т╨▓╨╡╨┤╨╕╤В╨╡ ╤Ж╨╕╤Д╤А╤Г 
 if '%choice%'=='5' = goto 5
 if '%choice%'=='6' = goto 6
 goto start
 :5
-echo Перейдите на сайт и вставьте ссылку в специальное поле, после чего скачайте Microsoft.GamingServices_"version"_neutral_~_8wekyb3d8bbwe.appxbundle. После чего установите 2 кликом
-echo Скопируйте ссылку, (не открывать) после нажатия вы будете перенаправленные на сайт где надо её ввести.
+echo ╨Я╨╡╤А╨╡╨╣╨┤╨╕╤В╨╡ ╨╜╨░ ╤Б╨░╨╣╤В ╨╕ ╨▓╤Б╤В╨░╨▓╤М╤В╨╡ ╤Б╤Б╤Л╨╗╨║╤Г ╨▓ ╤Б╨┐╨╡╤Ж╨╕╨░╨╗╤М╨╜╨╛╨╡ ╨┐╨╛╨╗╨╡, ╨┐╨╛╤Б╨╗╨╡ ╤З╨╡╨│╨╛ ╤Б╨║╨░╤З╨░╨╣╤В╨╡ Microsoft.GamingServices_"version"_neutral_~_8wekyb3d8bbwe.appxbundle. ╨Я╨╛╤Б╨╗╨╡ ╤З╨╡╨│╨╛ ╤Г╤Б╤В╨░╨╜╨╛╨▓╨╕╤В╨╡ 2 ╨║╨╗╨╕╨║╨╛╨╝
+echo ╨б╨║╨╛╨┐╨╕╤А╤Г╨╣╤В╨╡ ╤Б╤Б╤Л╨╗╨║╤Г, (╨╜╨╡ ╨╛╤В╨║╤А╤Л╨▓╨░╤В╤М) ╨┐╨╛╤Б╨╗╨╡ ╨╜╨░╨╢╨░╤В╨╕╤П ╨▓╤Л ╨▒╤Г╨┤╨╡╤В╨╡ ╨┐╨╡╤А╨╡╨╜╨░╨┐╤А╨░╨▓╨╗╨╡╨╜╨╜╤Л╨╡ ╨╜╨░ ╤Б╨░╨╣╤В ╨│╨┤╨╡ ╨╜╨░╨┤╨╛ ╨╡╤С ╨▓╨▓╨╡╤Б╤В╨╕.
 echo https://www.microsoft.com/store/productId/9MWPM2CQNLHN
 pause
 start https://store.rg-adguard.net/
@@ -87,20 +87,20 @@ goto exit
 goto start
 :6
 echo -------------------------
-echo загрузка gaming services
+echo ╨╖╨░╨│╤А╤Г╨╖╨║╨░ gaming services
 echo -------------------------
 cd C:\
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1YWhb7cw8IbO8jgAUDyQY0a6peniW1QTw', 'Microsoft.GamingServices_neutral___8wekyb3d8bbwe.AppxBundle')"
 echo -------------------------
-echo установка gaming services
+echo ╤Г╤Б╤В╨░╨╜╨╛╨▓╨║╨░ gaming services
 echo -------------------------
 powershell -ExecutionPolicy ByPass -command "add-appxpackage -path C:\Microsoft.GamingServices_neutral___8wekyb3d8bbwe.AppxBundle"
 del "C:\Microsoft.GamingServices_neutral___8wekyb3d8bbwe.AppxBundle"
 color 2
 echo -------
-echo успешно
+echo ╤Г╤Б╨┐╨╡╤И╨╜╨╛
 echo -------
 goto exit
 :exit
-echo надеюсь скрипт вам помог
+echo ╨╜╨░╨┤╨╡╤О╤Б╤М ╤Б╨║╤А╨╕╨┐╤В ╨▓╨░╨╝ ╨┐╨╛╨╝╨╛╨│
 pause
